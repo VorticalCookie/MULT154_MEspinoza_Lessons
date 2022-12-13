287883a2d8f7c4d153abd3ed437dc652309f497a
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2022-12-12 16:10:54 -0800

    Animation Lesson
    Animation Lesson

--- Assets/Scripts/Forest/NavPlayerMovement.cs
+++ Assets/Scripts/Forest/NavPlayerMovement.cs
@@ -9,12 +9,18 @@ public class NavPlayerMovement : MonoBehaviour
     Rigidbody rgBody = null;
     float trans = 0;
     float rotate = 0;
+    private Animator anim;
+    private Camera camera;
+    private Transform lookTarget;
 
     public delegate void DropHive(Vector3 pos);
     public static event DropHive DroppedHive;
     private void Start()
     {
         rgBody = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+        camera = GetComponentInChildren<Camera>();
+        lookTarget = GameObject.Find("HeadAimTarget").transform;
     }
     void Update()
     {
@@ -27,6 +33,8 @@ public class NavPlayerMovement : MonoBehaviour
         // The value is in the range -1 to 1
         float translation = Input.GetAxis("Vertical");
         float rotation = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("speed", translation);
 
         trans += translation;
         rotate += rotation;
@@ -42,5 +50,62 @@ public class NavPlayerMovement : MonoBehaviour
         Vector3 move = transform.forward * trans;
         rgBody.velocity = move * speed * Time.deltaTime;
         trans = 0;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Hazard"))
+        {
+            anim.SetTrigger("died");
+            StartCoroutine(ZoomOut());
+        }
+
+        else
+        {
+            anim.SetTrigger("twitchLeftEar");
+        }
+    }
+
+    IEnumerator ZoomOut()
+    {
+        const int ITERATIONS = 10;
+        for (int z = 0; z < ITERATIONS; z++)
+        {
+            camera.transform.Translate(camera.transform.forward * -1 * 10.0f / ITERATIONS);
+            yield return new WaitForSeconds(1.0f / ITERATIONS);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hazard"))
+        {
+            // lookTarget.position = other.transform.position;
+            StartCoroutine(LookAndLookAway(lookTarget.position, other.transform.position));
+        }
+
+    }
+    private IEnumerator LookAndLookAway(Vector3 targetPos, Vector3 hazardPos)
+    {
+        Vector3 targetDir = targetPos - transform.position;
+        Vector3 hazardDir = hazardPos - transform.position;
+
+        float angle = Vector2.Angle(new Vector2(targetPos.x, targetPos.z), new Vector2(hazardPos.x, hazardPos.z));
+
+        const int INTERVALS = 20;
+        const float INTERVAL = 0.5f / INTERVALS;
+
+        float angleInterval = angle / INTERVALS;
+        for (int i = 0; i < INTERVALS; i++)
+        {
+            lookTarget.RotateAround(transform.position, Vector3.up, -angleInterval);
+            yield return new WaitForSeconds(INTERVAL);
+        }
+
+        for (int i = 0; i < INTERVALS; i++)
+        {
+            lookTarget.RotateAround(transform.position, Vector3.up, angleInterval);
+            yield return new WaitForSeconds(INTERVAL);
+        }
     }
 }
